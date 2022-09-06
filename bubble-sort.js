5c648466ee3724bcb45875337ed020ccdab05c42
Srikar charan <srikarsuri@gmail.com>
2022-09-06 14:07:41 +0530

    Implement Bubble Sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,6 +1,15 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {}
+function bubbleSort(array) {
+  for (let j = 1; j < array.length; j++)
+    for (let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i + 1]) {
+        const temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+      }
+    }
+}
 
 bubbleSort(numbers);
 console.log(numbers);
