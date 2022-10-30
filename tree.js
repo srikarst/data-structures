036686fe71f78e1bad3d597966eb7afd8a0c24d4
Srikar charan <srikarsuri@gmail.com>
2022-10-30 21:34:01 +0530

    Implement DFS in Tree

--- tree.js
+++ tree.js
@@ -164,6 +164,48 @@ class BinarySearchTree {
 
     return this.BreadthFirstSearchR(queue, list);
   }
+  DFTPreOrder(currentNode, list) {
+    return traversePreOrder(this.root, []);
+  }
+  DFTPostOrder() {
+    return traversePostOrder(this.root, []);
+  }
+  DFTInOrder() {
+    return traverseInOrder(this.root, []);
+  }
+}
+
+function traversePreOrder(node, list) {
+  list.push(node.value);
+  if (node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePreOrder(node.right, list);
+  }
+  return list;
+}
+
+function traverseInOrder(node, list) {
+  if (node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.value);
+  if (node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
+
+function traversePostOrder(node, list) {
+  if (node.left) {
+    traversePostOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePostOrder(node.right, list);
+  }
+  list.push(node.value);
+  return list;
 }
 
 const tree = new BinarySearchTree();
@@ -181,6 +223,10 @@ console.log(JSON.stringify(traverseIteratively(tree.root)));
 
 console.log("BFS", tree.BreadthFirstSearch());
 console.log("BFS", tree.BreadthFirstSearchR([tree.root], []));
+console.log("DFSpre", tree.DFTPreOrder());
+console.log("DFSin", tree.DFTInOrder());
+console.log("DFSpost", tree.DFTPostOrder());
+
 //     9
 //  4     20
 //1  6  15  170
